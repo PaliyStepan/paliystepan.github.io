603c6149000a3a31360d9de127a17c17bd044f4c
Paliy Stepan <palstepweb@gmail.com>
2020-10-24 15:10:50 +0300

    delete trash

--- genotek/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-    $('.accordion-item__visible').on('click', function(event) {
-        $(this).toggleClass('active');
-        $(this).siblings('.accordion-item__hidden').slideToggle(350);
-    });
-});
