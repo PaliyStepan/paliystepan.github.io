c0575f6d69ed55fb5aac4fe51b83412e1f8acd01
Stepan Paliy <palstepweb@gmail.com>
2024-10-20 19:47:16 +0500

    geometry

--- present/assets/main-legacy-TbF0GENk.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-CW4Xt6Co.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
---
+++ presentation/assets/about-legacy-tEIcm8u7.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-DlpMbN-D.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
