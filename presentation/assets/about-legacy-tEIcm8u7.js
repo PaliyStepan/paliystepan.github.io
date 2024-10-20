2bd0e631f1d5d6255e7915ee835ae030e2e9d4cc
Stepan Paliy <palstepweb@gmail.com>
2024-10-20 22:11:27 +0500

    ghost  house

--- presentation/assets/about-legacy-tEIcm8u7.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-DlpMbN-D.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
---
+++ presentation/assets/scrollAnimation-legacy-DWLRNOft.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-DV5xyRiF.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
