6d8d2cd79164d4edc2a87ecb146b73a60a691d81
Stepan Paliy <palstepweb@gmail.com>
2025-01-08 15:41:52 +0500

    test

--- presentation/assets/scrollAnimation-QPtI7KtX.js
+++
@@ -1,1 +0,0 @@
-import"./main-DLTGb4b9.js";function t(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}export{t as __vite_legacy_guard};
--- presentation/assets/scrollAnimation-legacy-DWLRNOft.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-DV5xyRiF.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
