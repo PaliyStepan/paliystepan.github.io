8844a27ca0fee95e2969f5c6961d2ad19d52d268
Stepan Paliy <palstepweb@gmail.com>
2025-01-09 13:18:54 +0500

    страницы презентации

---
+++ presentation/assets/galaxy-legacy-rJiszTuv.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-Ccjxlg7v.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
--- presentation/assets/sea-legacy-B_ePUsNE.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-D_yuHU84.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
