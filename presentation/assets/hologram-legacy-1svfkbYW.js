eeb328d434708880f503147fc40124f25357767a
Stepan Paliy <palstepweb@gmail.com>
2025-01-09 13:04:11 +0500

    test

--- presentation/assets/hologram-legacy-1svfkbYW.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-BJFrBa6-.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
---
+++ presentation/assets/robot-legacy-B_ePUsNE.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-D_yuHU84.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
